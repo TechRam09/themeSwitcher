fd4f60e66fe99af3098e0bd52bc4706f8ce52048
TechRam09 <nsshreeram@gmail.com>
2024-06-13 10:22:16 +0530

    theme.js
    Context api, provider and accessing the context custom hook creation file

---
+++ src/context/theme.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+  themeMode: "light",
+  lightMode: () => {},
+  darkMode: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+  return useContext(ThemeContext);
+}
